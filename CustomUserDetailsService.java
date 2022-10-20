a9b6e6dca81778935877bf0e9be67dc322180211
anjalitripathi123 <116271569+anjalitripathi123@users.noreply.github.com>
2022-10-20 16:09:55 +0530

    CustomUserDetailsService

---
+++ CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.harshil.expensetracker.security;
+
+import com.harshil.expensetracker.entity.User;
+import com.harshil.expensetracker.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User existingUser= userRepository.findByEmail(email).orElseThrow(()->new UsernameNotFoundException("User not found for the email: "+email));
+
+        return new org.springframework.security.core.userdetails.User(existingUser.getEmail(),existingUser.getPassword(),new ArrayList<>());
+    }
+}
